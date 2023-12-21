9884569788b27428befb1f5551c474b9874f1770
ta2113 <ta2113@LV209>
2023-12-21 15:30:41 +0100

    Přidání tříd Škola a Žák

---
+++ Skola.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkolaKategorieZaciObj
+{
+    internal class Skola
+    {
+        private string nazev;
+        private List<List<Zak>> kategorie; // List který obsahuje listy s žáky. Každý list reprezentuje jednu kategorii.
+
+        public string Nazev { get { return nazev; } }
+        public List<List<Zak>> Kategorie { get { return kategorie; } set { kategorie = value; } }
+
+        public Skola(string nazev)
+        {
+            this.nazev = nazev;
+        }
+    }
+}
---
+++ Zak.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkolaKategorieZaciObj
+{
+    internal class Zak
+    {
+        private string jmeno;
+        private int misto;
+
+        public string Jmeno { get { return jmeno; } }
+        public int Misto { get { return misto; } set { misto = value; } }
+
+        public Zak(string jmeno)
+        {
+            this.jmeno = jmeno;
+        }
+    }
+}
