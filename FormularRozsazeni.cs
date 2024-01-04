fc5aefe6c36d69bd1a2e7f1dbc6461e54c1fc104
ta2113 <ta2113@LV209>
2024-01-04 15:41:02 +0100

    Drobná optimalizace a příprava na řazení žáků

--- FormularRozsazeni.cs
+++ FormularRozsazeni.cs
@@ -6,6 +6,8 @@
         private List<SolidBrush> barvyKategorii = new List<SolidBrush>();
         // List polí barev. Počet barev v poli na daném indexu odpovídá počtu kategorií vyplněné třídy
         private List<SolidBrush[]> barvyVyplnenychTrid = new List<SolidBrush[]>();
+        // TODO - vysvětlit
+        private List<Zak[,]> tridyZaku = new List<Zak[,]>();
         public FormularRozsazeni(IEnumerable<string> tridy)
         {
             InitializeComponent();
@@ -99,24 +101,19 @@
                 (int)(panelVykresleniRozsazeni.Width * 0.05),
                 (int)(panelVykresleniRozsazeni.Height * 0.05));
 
-            string[] dimensions = new string[2];
-            if (listbxVyberTrid.SelectedIndex == -1 && listbxVybraneTridy.SelectedIndex != -1 && listbxVyplneneTridy.SelectedIndex == -1)
+            int[] dimenze = extrahujDimenze();
+            int mistoSirka = (int)((panelVykresleniRozsazeni.Width * 0.9 - dimenze[0]) / dimenze[0]);
+            int mistoVyska = (int)((panelVykresleniRozsazeni.Height * 0.65 - dimenze[1]) / dimenze[1]);
+
+            // Vytvoří 2-dimenzionální pole o velikosti zvolené vyplňované třídy
+            if (listbxVyplneneTridy.SelectedIndex != -1)
             {
-                dimensions = listbxVybraneTridy.Text.Split('(')[1].Split('x');
+                tridyZaku.Add(new Zak[dimenze[0], dimenze[1]]);
             }
-            else if (listbxVyberTrid.SelectedIndex == -1 && listbxVybraneTridy.SelectedIndex == -1 && listbxVyplneneTridy.SelectedIndex != -1)
+
+            for (int r = 0; r < dimenze[1] ; r++)
             {
-                dimensions = listbxVyplneneTridy.Text.Split('(')[1].Split('x');
-            }
-
-            dimensions[1] = dimensions[1].Remove(dimensions[1].Length - 1);
-            int mistoSirka = (int)((panelVykresleniRozsazeni.Width * 0.9 - int.Parse(dimensions[1])) / int.Parse(dimensions[1]));
-            int mistoVyska = (int)((panelVykresleniRozsazeni.Height * 0.65 - int.Parse(dimensions[0])) / int.Parse(dimensions[0]));
-
-
-            for (int r = 0; r < int.Parse(dimensions[0]); r++)
-            {
-                for (int s = 0; s < int.Parse(dimensions[1]); s++)
+                for (int s = 0; s < dimenze[0]; s++)
                 {
                     g.FillRectangle(
                         ziskejBarvuDleKategorie(listbxVyplneneTridy.SelectedIndex != -1 ? listbxVyplneneTridy.SelectedIndex : -1, listbxVyplneneTridy.SelectedIndex != -1 ? (r * 2 + s) % barvyVyplnenychTrid[listbxVyplneneTridy.SelectedIndex].Length : -1),
@@ -125,6 +122,20 @@
                         mistoSirka, mistoVyska);
                 }
             }
+        }
+        private int[] extrahujDimenze()
+        {
+            string[] tmp = new string[2];
+            if (listbxVyberTrid.SelectedIndex == -1 && listbxVybraneTridy.SelectedIndex != -1 && listbxVyplneneTridy.SelectedIndex == -1)
+            {
+                tmp = listbxVybraneTridy.Text.Split('(')[1].Split('x');
+            }
+            else if (listbxVyberTrid.SelectedIndex == -1 && listbxVybraneTridy.SelectedIndex == -1 && listbxVyplneneTridy.SelectedIndex != -1)
+            {
+                tmp = listbxVyplneneTridy.Text.Split('(')[1].Split('x');
+            }
+            tmp[1] = tmp[1].Remove(tmp[1].Length - 1);
+            return new int[] { int.Parse(tmp[1]), int.Parse(tmp[0]) };
         }
 
         private void numupdownKategoriiNaTridu_ValueChanged(object sender, EventArgs e)
@@ -160,11 +171,17 @@
         private void btnVyplnit_Click(object sender, EventArgs e)
         {
             if (listbxVybraneTridy.SelectedIndex == -1) return;
-            // POZNÁMKA: Řádky níže jsou pouze dočasné jelikož slouží jako debug pomůcka, jestli
-            // jsou jednotlivá místa správně zabarvená. Zde se bude řešit řazení žáků - TODO
             listbxVyplneneTridy.Items.Add(listbxVybraneTridy.Items[listbxVybraneTridy.SelectedIndex]);
             listbxVybraneTridy.Items.RemoveAt(listbxVybraneTridy.SelectedIndex);
             barvyVyplnenychTrid.Add(barvyKategorii.ToArray());
+            // TODO - po kliknutí na tlačítlo se odznačí listbox a tedy nemůže pracovat funkce extrahujDimenze
+            //int[] tmp = extrahujDimenze();
+            //tridyZaku.Add(new Zak[tmp[0],tmp[1]]);
+            //vyplnZaky(tridyZaku.Count-1);
+        }
+        private void vyplnZaky(int indexTridy)
+        {
+            // TODO - Zde se bude vyplňovat specifikovaná třída na indexu zadaném v parametru funkce
         }
     }
 }
