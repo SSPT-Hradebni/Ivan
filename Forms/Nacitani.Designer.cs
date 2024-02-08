8d6c69e79289cb511bc2a2182d34367c517e1301
ultron01 <ultron01@faketube.cz>
2024-02-08 18:09:05 +0100

    Načítací okno

---
+++ Forms/Nacitani.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ivan
+{
+    partial class Nacitani
+    {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.loading = new System.Windows.Forms.Timer(this.components);
+            this.loadingBar = new System.Windows.Forms.ProgressBar();
+            this.lblTip = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loading
+            // 
+            this.loading.Enabled = true;
+            this.loading.Tick += new System.EventHandler(this.loading_Tick);
+            // 
+            // loadingBar
+            // 
+            this.loadingBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.loadingBar.Location = new System.Drawing.Point(0, 380);
+            this.loadingBar.Name = "loadingBar";
+            this.loadingBar.Size = new System.Drawing.Size(759, 42);
+            this.loadingBar.TabIndex = 0;
+            // 
+            // lblTip
+            // 
+            this.lblTip.BackColor = System.Drawing.Color.Transparent;
+            this.lblTip.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lblTip.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTip.Font = new System.Drawing.Font("Segoe UI Black", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTip.ForeColor = System.Drawing.Color.Black;
+            this.lblTip.Location = new System.Drawing.Point(0, 334);
+            this.lblTip.Name = "lblTip";
+            this.lblTip.Size = new System.Drawing.Size(759, 46);
+            this.lblTip.TabIndex = 1;
+            this.lblTip.Text = "Tip během načítání";
+            this.lblTip.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.BackgroundImage = global::Ivan.Properties.Resources.IvanLogo;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(759, 331);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Nacitani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Ivan.Properties.Resources.loadingBG;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(759, 422);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblTip);
+            this.Controls.Add(this.loadingBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Nacitani";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Načítání";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer loading;
+        private System.Windows.Forms.ProgressBar loadingBar;
+        private System.Windows.Forms.Label lblTip;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Forms/Nacitani.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ivan
+{
+    public partial class Nacitani : Form
+    {
+        public Nacitani()
+        {
+            InitializeComponent();
+        }
+
+        private void loading_Tick(object sender, EventArgs e)
+        {
+            loadingBar.Value = loadingBar.Value + 1;
+
+            // změna typu na základě aktuální hodnoty načítání
+            if(loadingBar.Value >= 0 && loadingBar.Value < 25)
+            {
+                lblTip.Text = "Ha, Ivan!";
+            }
+            else if(loadingBar.Value >= 25 && loadingBar.Value < 70)
+            {
+                lblTip.Text = "Ivan se načítá";
+            }
+            else if( loadingBar.Value >= 70 && loadingBar.Value < 95)
+            {
+                lblTip.Text = "Ivan už skoro je ...";
+            }
+            else if (loadingBar.Value >= 95 && loadingBar.Value < 100)
+            {
+                lblTip.Text = "... načtený!";
+            }
+
+            // pokud je program načten
+            if (loadingBar.Value >= 99)
+            {
+                loading.Enabled = false;
+            }
+        }
+    }
+}
